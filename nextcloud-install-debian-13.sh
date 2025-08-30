b14d4178f0f56bbb7944d8d702d09b4a67fb38c3
wootje <wootje-inbox@outlook.com>
2025-08-30 17:06:15 +0200

    Update nextcloud-install-debian-13.sh

--- nextcloud-install-debian-13.sh
+++ nextcloud-install-debian-13.sh
@@ -1,4 +1,4 @@
-set -euo pipefail
+#set -euo pipefail
 
 ##############################!/usr/bin/env bash
 
@@ -214,3 +214,4 @@ Data directory: ${NC_DATA}
 If you added a domain & e-mail, then a Let's Encrypt certificate will be created & HTTPS wil be available.
 ==========================================================
 INFO
+
